7dd77f032af5bbb69f5d7631ead261d3b8692346
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-04-20 10:06:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function calculator(number1, number2, operator) {
+    let result;
+    if (operator === '+') {
+        result = number1 + number2;
+    } else if (operator === '-') {
+        result = number1 - number2;
+    } else if (operator === '*') {
+        result = number1 * number2;
+    } else if (operator === '/') {
+        result = number1 / number2;
+    } else {
+        throw new RangeError('The operator must be an +, -, *, /');
+    }
+    return result;
+}
+
+try {
+    console.log(calculator(3, 9, '%'));
+} catch (err) {
+    console.log(err.message);
+}
